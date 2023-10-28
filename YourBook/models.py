1ae8db0053dba205a248b9d9305b07d3c247b6e0
Aaron <aaron.kwek@ui.ac.id>
2023-10-28 21:44:33 +0700

    menyesuaikan logic model

--- YourBook/models.py
+++ YourBook/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from main.models import Buku
 # Create your models here.
 class PinjamBuku(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    buku = models.ForeignKey(Buku, on_delete=models.CASCADE)
     petugas = models.CharField(max_length=255)
     judul_buku = models.CharField(max_length=255)
     durasi_pinjam = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -31,7 +31,8 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been successfully created!')
-            return redirect('YourBook:login'), redirect('wishlist:login'), redirect('review:login'),
+            return redirect('YourBook:login'), redirect('wishlist:login'), redirect('review:login'),            
+
     context = {'form':form}
     return render(request, 'register.html', context)
 

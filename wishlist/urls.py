59383808e3cbe9d12731b43fdc010c3110eec9ab
vinamyrnauli <vinasllgn@gmail.com>
2023-10-29 20:52:40 +0700

    add logout

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 
-from wishlist.views import show_main, add_wishlist, show_xml, show_json, show_json_by_id, show_xml_by_id, remove_wishlist, add_wishlist_ajax, get_wishlist_json
+from wishlist.views import show_main, add_wishlist, show_xml, show_json, show_json_by_id, show_xml_by_id, remove_wishlist, add_wishlist_ajax, get_wishlist_json, logout_user
 from django.urls import path
 
 app_name = 'wishlist'
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('add-wishlist', add_wishlist, name='add_wishlist'),
     path('xml/', show_xml, name='show_xml'), 
+    path('logout/', logout_user, name='logout'),
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 

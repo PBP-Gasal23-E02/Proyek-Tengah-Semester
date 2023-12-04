2b30a1bf561e69d67fa91086036da4525ef0a94f
Aaron <aaron.kwek@ui.ac.id>
2023-12-04 10:10:18 +0700

    try fix error deploy

--- main/models.py
+++ main/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 class Buku(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
     Text = models.IntegerField(null=True, blank=True)
-    Type = models.CharField(null=True, blank=True, max_length=255)
-    Issued = models.CharField(null=True, blank=True, max_length=255)
-    Title = models.CharField(null=True, blank=True, max_length=255)
-    Language = models.CharField(null=True, blank=True, max_length=255)
-    Authors = models.CharField(null=True, blank=True, max_length=255)
-    Subjects = models.CharField(null=True, blank=True, max_length=255)
-    LoCC = models.CharField(null=True, blank=True, max_length=255)
-    Bookshelves = models.CharField(default="Cookbooks and Cooking", max_length=255)
+    Type = models.TextField(null=True, blank=True, max_length=255)
+    Issued = models.TextField(null=True, blank=True, max_length=255)
+    Title = models.TextField(null=True, blank=True, max_length=255)
+    Language = models.TextField(null=True, blank=True, max_length=255)
+    Authors = models.TextField(null=True, blank=True, max_length=255)
+    Subjects = models.TextField(null=True, blank=True, max_length=255)
+    LoCC = models.TextField(null=True, blank=True, max_length=255)
+    Bookshelves = models.TextField(default="Cookbooks and Cooking", max_length=255)
 
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)

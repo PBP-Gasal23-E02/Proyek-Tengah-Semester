12538b53b932c0ee42398d6f7529e9ff1a564842
Aaron <aaron.kwek@ui.ac.id>
2023-12-04 18:55:01 +0700

    reset file migrations

--- main/models.py
+++ main/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 class Buku(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Text = models.IntegerField(null=True, blank=True)
-    Type = models.TextField(null=True, blank=True, max_length=255)
-    Issued = models.TextField(null=True, blank=True, max_length=255)
-    Title = models.TextField(null=True, blank=True, max_length=255)
-    Language = models.TextField(null=True, blank=True, max_length=255)
-    Authors = models.TextField(null=True, blank=True, max_length=255)
-    Subjects = models.TextField(null=True, blank=True, max_length=255)
-    LoCC = models.TextField(null=True, blank=True, max_length=255)
-    Bookshelves = models.TextField(default="Cookbooks and Cooking", max_length=255)
+    Text = models.TextField(null=True, blank=True)
+    Type = models.TextField(null=True, blank=True)
+    Issued = models.TextField(null=True, blank=True)
+    Title = models.TextField(null=True, blank=True)
+    Language = models.TextField(null=True, blank=True)
+    Authors = models.TextField(null=True, blank=True)
+    Subjects = models.TextField(null=True, blank=True)
+    LoCC = models.TextField(null=True, blank=True)
+    Bookshelves = models.TextField(default="Cookbooks and Cooking")
 
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
